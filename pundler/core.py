991718ab04c5781aae505e919c1ebbfb28307874
Mike Steder <steder@gmail.com>
2014-05-31 08:49:48 -0500

    working on refactoring to deal with multiple files in a cleaner (more OO) way

--- pundler/core.py
+++ pundler/core.py
@@ -1,7 +1,9 @@
 import argparse
 from collections import OrderedDict
+import glob
 import logging
 import os
+import sys
 import textwrap
 
 from pip.index import PackageFinder
@@ -24,80 +26,118 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 
-def get_requirement_file():
+def get_requirement_files():
     """
     Get the "best" requirements file we can find
     """
-    for filename in settings.REQUIREMENTS_SOURCE_FILES:
-        if os.path.exists(filename):
-            return filename
-    logger.warn(
-        textwrap.dedent(
-            """Sorry, I couldn't find a requirements.yml or
-            requirements.in!""")
-    )
+    paths = []
+    for regex in settings.REQUIREMENTS_SOURCE_GLOBS:
+        paths.extend(glob.glob(regex))
+    return paths
 
 
 def get_requirements(filename):
-    if filename is not None:
-        logger.info("processing %s" % filename)
-        with open(filename, "r") as f:
-            for line in f.readlines():
-                line = line.strip()
-                yield line
+    logger.info("processing %s" % filename)
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            yield line
 
 
-def install(args, lock_filename="requirements.txt"):
-    deps = OrderedDict()
+class Pundler(object):
+    def __init__(self, upgrade=False):
+        self.deps = OrderedDict()
+        self.args = []
+        self.upgrade = upgrade
 
-    filename = get_requirement_file()
-    for line in get_requirements(filename):
-        line = line.strip()
-        deps[line] = []
+    def process_requirements(self, filename):
+        for line in get_requirements(filename):
+            line = line.strip()
+            if line.startswith("-"):
+                self.args.append(line)
+                continue
 
-        requirement_set = RequirementSet(
-            build_dir=build_prefix,
-            src_dir=src_prefix,
-            download_dir=None)
+            self.deps[line] = []
 
-        requirement = InstallRequirement.from_line(line, None)
+            print("upgrade?:", self.upgrade)
+            requirement_set = RequirementSet(
+                build_dir=build_prefix,
+                src_dir=src_prefix,
+                download_dir=None,
+                upgrade=self.upgrade,
+            )
 
-        requirement_set.add_requirement(requirement)
+            requirement = InstallRequirement.from_line(line, None)
 
-        install_options = []
-        global_options = []
-        # TODO: specify index_urls from optional requirements.yml
-        finder = PackageFinder(
-            find_links=[],
-            index_urls=["http://pypi.python.org/simple/"]
+            requirement_set.add_requirement(requirement)
+
+            install_options = []
+            global_options = []
+            # TODO: specify index_urls from optional requirements.yml
+            finder = PackageFinder(
+                find_links=[],
+                index_urls=["http://pypi.python.org/simple/"]
+            )
+
+            requirement_set.prepare_files(finder, force_root_egg_info=False, bundle=False)
+            requirement_set.install(install_options, global_options)
+
+            for package in requirement_set.requirements.values():
+                if package.satisfied_by.has_metadata('PKG-INFO'):
+                    dep = "%s==%s" % (package.name, package.installed_version)
+                    print("values:", dep)
+                    self.deps[line].append(dep)
+
+            for package in requirement_set.successfully_installed:
+                dep = "%s==%s" % (package.name, package.installed_version)
+                print("installed:", dep)
+                self.deps[line].append(dep)
+
+            self.deps[line] = set(self.deps[line])
+
+        package_set = set([])
+
+        lock_filename = filename.replace(".in", ".txt")
+        with open(lock_filename, "w") as output:
+            output.write("# this file generated from '%s' by pundler:\n" % (filename,))
+            for argument in self.args:
+                output.write("%s\n"%(argument,))
+                output.write("\n")
+            for requested_package in self.deps:
+                output.write("# requirement '%s' depends on:\n" % (requested_package,))
+                for dependency in self.deps[requested_package]:
+                    logger.info("dependency %s" % dependency)
+                    if dependency not in package_set:
+                        dependency = dependency.lower()
+                        package_set.add(dependency)
+                        output.write("%s\n" % (dependency,))
+                    else:
+                        output.write("#%s\n" % (dependency,))
+                output.write("\n")
+
+
+
+def install(args):
+    filenames = get_requirement_files()
+    if not filenames:
+        logger.warn(
+            textwrap.dedent(
+            """
+            Sorry, I couldn't find any requirements files!
+            I tried the following globs:
+            """) + "\n - ".join([""] + settings.REQUIREMENTS_SOURCE_GLOBS)
         )
+        sys.exit(-2)
+    for filename in filenames:
+        pundler = Pundler(upgrade=args.upgrade)
+        pundler.process_requirements(filename)
 
-        requirement_set.prepare_files(finder, force_root_egg_info=False, bundle=False)
-        requirement_set.install(install_options, global_options)
 
-        for package in requirement_set.requirements.values():
-            deps[line].append("%s==%s" % (package.name, package.installed_version))
-
-        for package in requirement_set.successfully_installed:
-            deps[line].append("%s==%s" % (package.name, package.installed_version))
-
-        deps[line] = set(deps[line])
-
-    package_set = set([])
-
-    with open(lock_filename, "w") as output:
-        output.write("# this file generated from '%s' by pundler:\n" % (filename,))
-        for requested_package in deps:
-            output.write("# requirement '%s' depends on:\n" % (requested_package,))
-            for dependency in deps[requested_package]:
-                logger.info("dependency %s" % dependency)
-                if dependency not in package_set:
-                    dependency = dependency.lower()
-                    package_set.add(dependency)
-                    output.write("%s\n" % (dependency,))
-                else:
-                    output.write("#%s\n" % (dependency,))
-            output.write("\n")
+def update(args):
+    args.upgrade = True
+    install(args)
 
 
 def get_parser():
@@ -112,9 +152,12 @@ def get_parser():
                                        help='additional help')
     install_parser = subparsers.add_parser('install')
     install_parser.set_defaults(func=install)
+    update_parser = subparsers.add_parser('update')
+    update_parser.set_defaults(func=update)
     return parser
 
 
 def main():
     args = get_parser().parse_args()
+    args.upgrade = False
     args.func(args)
